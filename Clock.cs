a557134f13f04596f78c9c7df2b1a667da283ad5
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:32:34 +0200

    Initial commit

---
+++ Clock.cs
@@ -0,0 +1,25 @@
+﻿namespace ClockV10
+{
+    public class Clock
+    {
+        private int _timeInMinutesSince0000;
+        public int timeInMinutes
+        {
+            get{return _timeInMinutesSince0000%60;}
+        }
+        public int timeInHours
+        {
+            get{return _timeInMinutesSince0000/60;}
+        }
+        public void SetTime(int hours, int minutes)
+        {
+            _timeInMinutesSince0000 = 60*hours+minutes;
+        }
+        public void DisplayTime()
+        {   
+            System.Console.WriteLine($"The time is {timeInHours} {timeInMinutes}");
+        }
+        
+        
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Clock myClock = new Clock();
+            myClock.DisplayTime();
+            myClock.SetTime(15,35);
+            myClock.DisplayTime();
+            
+
+
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
